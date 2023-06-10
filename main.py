45502022874f9b9d827502e0d1b36837c5317288
Giorgi <88037076+GiorgiBG@users.noreply.github.com>
2023-06-10 12:50:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 import requests
+from test import SendMail
 
 BLOG_API = "https://api.npoint.io/b1aa3a65fe53d4e0e332"
 
@@ -10,7 +11,7 @@ response = requests.get(BLOG_API)
 
 app = Flask(__name__, static_folder='static')
 data = response.json()
-@app.route("/index.html")
+@app.route("/")
 def root():
     return render_template("index.html", text = data)
 
@@ -18,8 +19,10 @@ def root():
 def about():
     return render_template('about.html')
 
-@app.route("/contact-me")
+@app.route("/contact-me" )
 def contact_me():
+    print(request.method)
+
     return render_template('contact.html')
 
 @app.route("/post/<int:index>")
@@ -28,6 +31,12 @@ def post(index):
         if item['id'] == index:
             return render_template('post.html', blog=item)
 
+@app.route("/form-entry", methods=['POST'])
+def receive_data():
+    if request.method == "POST":
+        send = SendMail(mobile=request.form['mobile'],body=request.form["message"], name=request.form['name'])
+        send.send_mail()
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
